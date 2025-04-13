ca49507c484b09ba47f94d91b7afd37414673f5c
OrMacc <88333840+OrMacc@users.noreply.github.com>
2025-04-13 21:10:38 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { runServer } from "mongo-mcp";
-
-const config = {
-  mongoUri: process.env.MONGO_URI || "mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTER>.mongodb.net/<DATABASE>?retryWrites=true&w=majority&authSource=admin",
-};
-
-runServer(config);
